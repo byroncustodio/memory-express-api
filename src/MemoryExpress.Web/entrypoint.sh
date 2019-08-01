f831680e953e1db3bb4c23db50c5a9318e5c40ef
Byron Custodio <bcustodio@outlyertech.com>
2019-07-31 22:21:51 -0700

    Finished setting up dev environment pipeline

--- src/MemoryExpress.Web/entrypoint.sh
+++ src/MemoryExpress.Web/entrypoint.sh
@@ -1,24 +1,17 @@
 #!/bin/bash
 
 set -e
-echo "Test"
-echo $1
-#dotnet restore
-
-# until dotnet ef database update -p ../MemoryExpress.Infrastructure/MemoryExpress.Infrastructure.csproj -s ./MemoryExpress.Web.csproj; do
-# >&2 echo "DB is starting up"
-# sleep 1
-# done
-
-#>&2 echo "DB is up - executing command"
-#exec $run_cmd
-
-if [[ "$1" = "Development" ]]; then
-    echo "Dev"
-    dotnet watch -p MemoryExpress.Web.csproj run
-fi
+run_cmd="dotnet watch -p MemoryExpress.Web.csproj run"
 
 if [[ "$1" = "Production" ]]; then
-    echo "Prod"
-    dotnet MemoryExpress.Web.dll 
+    run_cmd="dotnet run -p MemoryExpress.Web.csproj"
 fi
+
+>&2 echo "DB is starting up"
+until dotnet ef database update -p ../MemoryExpress.Infrastructure/MemoryExpress.Infrastructure.csproj -s ./MemoryExpress.Web.csproj;
+do
+sleep 1
+done
+
+>&2 echo "DB is up - executing command"
+exec $run_cmd
